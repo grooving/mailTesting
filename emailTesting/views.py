f18e6bc8be2aa79905437c5e97f2b7eab3ee4845
Juan Carlos Utrilla <utri1990@gmail.com>
2019-04-05 22:59:41 +0200

    Send a custom PDF (First version)

--- emailTesting/views.py
+++ emailTesting/views.py
@@ -9,17 +9,64 @@ from django.http import HttpResponse
 
 
 # Documentation based: https://docs.djangoproject.com/en/2.2/topics/email/
+from io import BytesIO
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+
+
+# ======================= CLASE reportePDF =========================
+
+def generate_report():
+
+    # Create the HTTP Response headers with PDF
+
+    # response = HttpResponse(content_type='application/pdf')
+    # response['Content-Disposition'] = 'attachment; filename =Nombre-fichero-report.pdf'
+
+    # Create the PDF object, using the BytesIO object as its "file."
+
+    buffer = BytesIO()
+    c = canvas.Canvas(buffer, pagesize=A4)
+
+    # Header
+
+    c.setLineWidth(.3)
+    c.setFont('Helvetica', 22)
+    c.drawString(30, 750, 'Grooving')
+    c.setFont('Helvetica', 12)
+    c.drawString(30, 735, 'Report')
+
+    c.setFont('Helvetica-Bold', 12)
+    c.drawString(480, 750, "06/06/2019")
+    c.line(460, 747, 560, 747)
+
+    # Save pdf
+
+    c.save()
+
+    # Get the value of BytesIO buffer and write response
+
+    pdf = buffer.getvalue()
+    buffer.close()
+
+    return pdf
+
+
+# A este método deberá llegarle: idOffer,
 
 def send_email_for_grooving(request):
 
-    # Forma 2. con sendmail
+    # Form 2 (Final): calling each variable & adding a PDF file
+
+    pdf = generate_report()
 
     email = EmailMessage()
-    email.subject = 'Hello'
-    email.body = 'Body goes here'                     # Aquí va el contenido HTML
+    email.subject = 'Payment made'
+    email.body = 'This is the payment for your performance'                     # Aquí va el contenido HTML
     email.from_email = 'Grooving <no-reply@grupogrooving.com>'
     email.to = ['utri1990@gmail.com']
-    email.attach_file('emailTesting/invoice.pdf')
+    # email.attach_file('emailTesting/invoice.pdf')
+    email.attach('bill.pdf', pdf, 'application/pdf')
     email.send()
 
     return HttpResponse("Correo enviado")
